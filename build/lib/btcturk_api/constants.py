ef635fa9f31830d47ae8d94ca5a153f98ba91d52
Mirac Baydemir <omermirac59@gmail.com>
2020-02-12 11:24:25 +0300

    RetrackFiles

--- build/lib/btcturk_api/constants.py
+++
@@ -1,4 +0,0 @@
-CURRENCY_SYMBOLS = ['TRY']
-CRYPTO_SYMBOLS = ['BTC', 'EOS', 'ETH', 'DASH', 'ATOM', 'USDT', 'LTC', 'XLM', 'XRP', 'NEO', 'LINK']
-TRADE_TYPES = ['buy', 'sell']
-DEPOSIT_OR_WITHDRAWAL = ['deposit', 'withdrawal']
