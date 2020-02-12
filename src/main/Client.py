b230870b2a128741c3e5626017b374205cafc028
Mirac Baydemir <omermirac59@gmail.com>
2020-02-12 03:54:09 +0300

    Add Feature: Authentication System

--- src/main/Client.py
+++ src/main/Client.py
@@ -1,2 +1,74 @@
+import base64
+import hashlib
+import hmac
+
+import requests
+import time
+
 class Client:
-    pass
+
+    API_BASE = "https://api.btcturk.com"
+    API_ENDPOINT_AUTH = "/api/v1/"
+    API_ENDPOINT_NON_AUTH = "/api/v2/"
+    API_ENDPOINT_TRANSACTIONS = "/api/v1/users/transactions/"
+
+    def __init__(self, api_key=None, api_secret=None):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.authenticated = False
+        self.session = None
+
+        self._init_session()
+
+        if api_key and api_secret:
+            self.authenticate()
+
+    def _init_session(self):
+        session = requests.session()
+        headers = {
+            "Content-Type": "application/json"
+        }
+        session.headers.update(headers)
+        self.session = session
+
+    def _create_public_endpoint_url(self, endpoint):
+        return f"{self.API_BASE}{self.API_ENDPOINT_NON_AUTH}{endpoint}"
+
+    def _create_auth_endpoint_url(self, endpoint):
+        return f"{self.API_BASE}{self.API_ENDPOINT_AUTH}{endpoint}"
+
+    def _create_signature(self):
+        api_secret = base64.b64decode(self.api_secret)
+        stamp = str(int(time.time()) * 1000)
+        data = "{}{}".format(self.api_key, stamp).encode('utf-8')
+        signature = hmac.new(api_secret, data, hashlib.sha256).digest()
+        signature = base64.b64encode(signature)
+        return signature
+
+    def _update_session_headers(self, **kwargs):
+        signature = self._create_signature()
+        headers = {
+            "X-Stamp": str(int(time.time()) * 1000),
+            "X-Signature": str(signature.decode('utf-8')),
+        }
+
+        for key, value in kwargs:
+            headers[key] = value
+
+        self.session.headers.update(headers)
+
+    # Signature bug: report to btcturk-trader github page
+    def authenticate(self):
+        url = self._create_auth_endpoint_url('users/balances')
+        signature = self._create_signature()
+        headers = {
+            "X-PCK": self.api_key,
+            "X-Stamp": str(int(time.time()) * 1000),
+            "X-Signature": str(signature.decode('utf-8')),
+        }
+        status = requests.get(url, headers=headers).status_code
+
+        if status == 200:
+            # Authentication successful, update session header
+            self.authenticated = True
+            self.session.headers.update({"X-PCK": self.api_key})
